ef5a7dad2bad512a1eee8719032431b9a8f97b19
BlasterZer0 <35616590+BlasterZer0@users.noreply.github.com>
2020-11-03 15:14:48 -0400

    Add files via upload

--- api/enviar.php
+++ api/enviar.php
@@ -29,31 +29,47 @@ try {
         EOT;
         $mail->send();
     }  
-    if ($mail->addAddress('mailsenderprojectgit@gmail.com', 'Comprobante')) {
-                    $mail->setFrom('mailsenderprojectgit@gmail.com', 'Comprobante');
-                    //Content
-                    $mail->isHTML(true);                                  // Set email format to HTML
-                    $mail->Subject = <<<EOT
-                    {$_POST['Asunto']}
-                    EOT;
-                    $mail->Body = <<<EOT
-                    Nombre: {$_POST['Nombre']}
-                    <br>
-                    Correo electrónico: {$_POST['Email']}
-                    <br>
-                    Dirección: {$_POST['Direccion']}
-                    <br>
-                    Teléfono: {$_POST['Telefono']}
-                    <br>
-                    Descripción del problema: {$_POST['Descripcion']}
-                    EOT;
-                    $mail->send();
-                    header( "Location: /success.html" );
-        }
         } catch (Exception $e) {
             echo 'Message could not be sent.';
             echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'mailsenderprojectgit@gmail.com';                 // SMTP username
+    $mail->Password = '369246489';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    if ($mail->addAddress('mailsenderprojectgit@gmail.com', 'Comprobante')) {
+        $mail->setFrom('mailsenderprojectgit@gmail.com', 'Comprobante');
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = <<<EOT
+        {$_POST['Asunto']}
+        EOT;
+        $mail->Body = <<<EOT
+        Nombre: {$_POST['Nombre']}
+        <br>
+        Correo electrónico: {$_POST['Email']}
+        <br>
+        Dirección: {$_POST['Direccion']}
+        <br>
+        Teléfono: {$_POST['Telefono']}
+        <br>
+        Descripción del problema: {$_POST['Descripcion']}
+        EOT;
+        $mail->send();
+        header( "Location: /success.html" );
+}
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
 
 
 
