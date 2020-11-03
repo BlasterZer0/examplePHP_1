ee7b1f5ea74c6f3df0147ddbed2b4d206812b593
BlasterZer0 <35616590+BlasterZer0@users.noreply.github.com>
2020-11-03 16:46:50 -0400

    Update enviar.php

--- api/enviar.php
+++ api/enviar.php
@@ -41,8 +41,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'mailsenderprojectgit@gmail.com';                 // SMTP username
-    $mail->Password = '369246489';                           // SMTP password
+    $mail->Username = 'USER';                 // SMTP username
+    $mail->Password = 'PASSWORD';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
