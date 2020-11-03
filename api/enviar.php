9396754391171889c74a58f4443cdebba1f18725
BlasterZer0 <35616590+BlasterZer0@users.noreply.github.com>
2020-11-03 16:48:13 -0400

    Add files via upload

--- api/enviar.php
+++ api/enviar.php
@@ -6,6 +6,13 @@ use PHPMailer\PHPMailer\Exception;
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+require 'phpdotenv/src/Dotenv.php';
+require 'phpdotenv/src/Validator.php'
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+$USER = $_ENV('USER');
+$PASSWORD = $_ENV('PASSWORD');
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
@@ -14,8 +21,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'mailsenderprojectgit@gmail.com';                 // SMTP username
-    $mail->Password = '369246489';                           // SMTP password
+    $mail->Username = 'USER';                 // SMTP username
+    $mail->Password = 'PASSWORD';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
@@ -41,8 +48,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'USER';                 // SMTP username
-    $mail->Password = 'PASSWORD';                           // SMTP password
+    $mail->Username = 'mailsenderprojectgit@gmail.com';                 // SMTP username
+    $mail->Password = '369246489';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
