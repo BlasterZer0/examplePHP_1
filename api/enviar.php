62ea45559a7bf3da0f842206058a7c208cbd571f
Braulio Rojas <brauliojrojas@gmail.com>
2020-11-05 14:10:18 -0400

    Update enviar.php

--- api/enviar.php
+++ api/enviar.php
@@ -13,8 +13,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
 
-$GMAILUSER = $_ENV['GMAILUSER'];
-$GMAILPASSWORD = $_ENV['GMAILPASSWORD'];
+$GMAILUSER = $_SERVER['GMAILUSER'];
+$GMAILPASSWORD = $_SERVER['GMAILPASSWORD'];
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
@@ -23,8 +23,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'mailsenderprojectgit@gmail.com';                 // SMTP username
-    $mail->Password = '369246489';                           // SMTP password
+    $mail->Username = '$GMAILUSER';                 // SMTP username
+    $mail->Password = '$GMAILPASSWORD';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
