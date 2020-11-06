c7b94c5a370571dbac134968426702d870a66e79
Braulio Rojas <brauliojrojas@gmail.com>
2020-11-06 14:11:25 -0400

    Update enviar.php

--- api/enviar.php
+++ api/enviar.php
@@ -65,6 +65,48 @@ try {
         Teléfono: {$_POST['Telefono']}
         <br>
         Descripción del problema: {$_POST['Descripcion']}
+
+        $message  = "<html><body>";
+   
+        $message .= "<table width='100%' bgcolor='#e0e0e0' cellpadding='0' cellspacing='0' border='0'>";
+        
+        $message .= "<tr><td>";
+        
+        $message .= "<table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#fff; font-family:Verdana, Geneva, sans-serif;'>";
+         
+        $message .= "<thead>
+           <tr height='80'>
+            <th colspan='4' style='background-color:#f5f5f5; border-bottom:solid 1px #bdbdbd; font-family:Verdana, Geneva, sans-serif; color:#333; font-size:34px;' >Programacion.net</th>
+           </tr>
+           </thead>";
+         
+        $message .= "<tbody>
+           <tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
+            <td style='background-color:#00a2d1; text-align:center;'><a href='http://programacion.net/articulos/c' style='color:#fff; text-decoration:none;'>C</a></td>
+            <td style='background-color:#00a2d1; text-align:center;'><a href='http://programacion.net/articulos/php' style='color:#fff; text-decoration:none;'>PHP</a></td>
+            <td style='background-color:#00a2d1; text-align:center;'><a href='http://programacion.net/articulos/ASP' style='color:#fff; text-decoration:none;' >ASP</a></td>
+            <td style='background-color:#00a2d1; text-align:center;'><a href='http://programacion.net/articulos/java' style='color:#fff; text-decoration:none;' >Java</a></td>
+           </tr>
+           
+           <tr>
+            <td colspan='4' style='padding:15px;'>
+             <p style='font-size:20px;'>Hi' ".$full_name.",</p>
+             <hr />
+             <p style='font-size:25px;'>Sending HTML eMail using PHPMailer</p>
+             <img src='https://4.bp.blogspot.com/-rt_1MYMOzTs/VrXIUlYgaqI/AAAAAAAAAaI/c0zaPtl060I/s1600/Image-Upload-Insert-Update-Delete-PHP-MySQL.png' alt='Sending HTML eMail using PHPMailer in PHP' title='Sending HTML eMail using PHPMailer in PHP' style='height:auto; width:100%; max-width:100%;' />
+             <p style='font-size:15px; font-family:Verdana, Geneva, sans-serif;'>".$text_message.".</p>
+            </td>
+           </tr>
+           
+           </tbody>";
+         
+        $message .= "</table>";
+        
+        $message .= "</td></tr>";
+        $message .= "</table>";
+        
+        $message .= "</body></html>";
+
         EOT;
         $mail->send();
     
